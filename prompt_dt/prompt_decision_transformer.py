0ce3d8291ec2ee56643e8c5a4d06fef98da2764b
mengsong16 <mes050@ucsd.edu>
2023-04-09 15:11:31 -0700

    add heavy goal encoder

--- prompt_dt/prompt_decision_transformer.py
+++ prompt_dt/prompt_decision_transformer.py
@@ -9,6 +9,24 @@ import transformers
 
 from prompt_dt.trajectory_gpt2 import GPT2Model
 
+class HeavyEncoder(nn.Module):
+    def __init__(self, input_dim, output_dim,
+                 hidden_dim=512):
+        super().__init__()
+
+        self.MLP = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim)
+        )
+
+    def forward(self, x):
+        x = self.MLP(x)
+        
+        return x
+    
 class PromptDecisionTransformer(nn.Module):
 
     def __init__(
@@ -70,7 +88,8 @@ class PromptDecisionTransformer(nn.Module):
             self.prompt_embed_state = torch.nn.Linear(self.state_dim, hidden_size)
             self.prompt_embed_action = torch.nn.Linear(self.act_dim, hidden_size)
         elif self.prompt_method == "goal_prompt" or self.prompt_method == "goal_diff_prompt":
-            self.goal_prompt_embed = torch.nn.Linear(self.goal_dim, hidden_size)
+            #self.goal_prompt_embed = torch.nn.Linear(self.goal_dim, hidden_size)
+            self.goal_prompt_embed = HeavyEncoder(input_dim=self.goal_dim, output_dim=hidden_size)
         elif self.prompt_method == "goal_state_prompt":
             self.goal_state_prompt_embed = torch.nn.Linear(self.state_dim, hidden_size)
         elif self.prompt_method == "goal_learned_prompt":
