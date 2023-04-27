5f4eb4c1e601870388584f298006784aba221cd2
mengsong16 <mes050@ucsd.edu>
2023-04-27 03:08:27 -0700

    remove metaworld from staging area

--- envs/metaworld/metaworld/envs/__init__.py
+++
@@ -1,8 +0,0 @@
-from metaworld.envs.mujoco.env_dict import (ALL_V2_ENVIRONMENTS_GOAL_HIDDEN,
-                                            ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE,
-                                            ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE_RANDOM
-                                            )
-
-__all__ = ['ALL_V2_ENVIRONMENTS_GOAL_HIDDEN',
-           'ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE',
-           'ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE_RANDOM']
