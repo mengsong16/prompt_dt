e8b68b87b6a5b9176de96f39969aaf38d04f3a5a
mengsong16 <mes050@ucsd.edu>
2023-05-01 21:15:15 -0700

    align hyperparameters to pdt

--- prompt_dt/pdt_main.py
+++ prompt_dt/pdt_main.py
@@ -456,8 +456,8 @@ if __name__ == '__main__':
     #experiment_mix_env(config_filename="cheetah_dir.yaml", mode="train") # mode: ['train', 'eval', 'demo']
     #experiment_mix_env(config_filename="cheetah_vel.yaml", mode="train")
     #experiment_mix_env(config_filename="ant_dir.yaml", mode="train")
-    experiment_mix_env(config_filename="ML1-pick-place-v2.yaml", mode="train")
-    #experiment_mix_env(config_filename="ML1-reach-v2.yaml", mode="train")
+    #experiment_mix_env(config_filename="ML1-pick-place-v2.yaml", mode="train")
+    experiment_mix_env(config_filename="ML1-reach-v2.yaml", mode="train")
     #experiment_mix_env(config_filename="ML1-push-v2.yaml", mode="train")
     
     
